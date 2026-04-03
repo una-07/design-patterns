f9e60516696edd34fca84543c1e932cf4419c913
una-07 <esirkepulpan8@gmail.com>
2026-04-03 17:07:19 +0500

    Create audio_system.py

---
+++ facade/audio_system.py
@@ -0,0 +1,9 @@
+class AudioSystem:
+    def on(self):
+        print("Аудио жүйе қосылды")
+
+    def off(self):
+        print("Аудио жүйе өшірілді")
+
+    def set_volume(self, volume):
+        print(f"Дыбыс деңгейі: {volume}")
