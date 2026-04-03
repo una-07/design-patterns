0c5e6e192d385895bd88e26069ece7759fd87e06
una-07 <esirkepulpan8@gmail.com>
2026-04-03 17:06:31 +0500

    Create  tv.py

---
+++ facade/tv.py
@@ -0,0 +1,9 @@
+class TV:
+    def on(self):
+        print("TV қосылды")
+
+    def off(self):
+        print("TV өшірілді")
+
+    def set_channel(self, channel):
+        print(f"TV канал {channel} қойылды")
